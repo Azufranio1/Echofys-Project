a5411c5e15a7a8e15212556bc7e30921214c70b6
Azufranio1 <carlosaliaga037@gmail.com>
2026-04-24 16:20:27 -0400

    cambios en la estructura, bye mysql y mongo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,23 +6,20 @@ import musicRoutes from './routes/musicRoutes';
 import authRoutes from './routes/authRoutes';
 import { connectRedis } from './lib/redis';
 
-import { Song } from './models/Song';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080; 
 
 app.use(cors());
 app.use(express.json());
 
-// 1. Conexión a Mongo
-const mongoUri = process.env.MONGO_URI || 'mongodb://mongo:27017/echofy';
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://Echofy-Admin:Mr_Master123@cluster0.iz362.mongodb.net/echofy_db';
+
 mongoose.connect(mongoUri)
-  .then(() => console.log('✅ Conectado a MongoDB'))
+  .then(() => console.log('✅ Conectado a MongoDB (Remoto)'))
   .catch(err => console.error('❌ Error de conexión:', err));
 
 connectRedis().then(() => console.log('✅ Conectado a Redis'));
-// 2. REGISTRO DE RUTAS
-// Todas las rutas que definas en musicRoutes.ts se le sumará el prefijo /api/songs
 app.use('/api/songs', musicRoutes);
 app.use('/api/auth', authRoutes);
 
